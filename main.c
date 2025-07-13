da93a7be412f0709cc5f592a43ad861a106cef4d
ajassi9751 <ajassi9751@gmail.com>
2025-07-13 14:55:56 -0400

    A terrible implementation of flags

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
+#include<stdbool.h>
 
 void printBianary (unsigned char byte) {
 	int i;
@@ -11,62 +11,77 @@ void printBianary (unsigned char byte) {
 
 
 int main (int argc, char *argv[]) {
-	char path[256];
+	int opt;
+	bool isInteger = false;
+	while (opt=getopt(argc,argv,"i")!=-1) {
+		switch (opt) {
+			case 'i':
+				isInteger=true;
+				break;
+			default:
+				perror("Usage: catb [-i] <filename> ...\n");
+				return EXIT_FAILURE;
+		}
+	}	
 	if (argc<2) {
-		perror("Usage: catb <filename> <filename> <filename> ...");
+		perror("Expected a valid filename or path");
 		return EXIT_FAILURE;
 	}
 	FILE *readee;
-	unsigned char byte;
 	int i;
-	for (i=1;i<argc;++i) {	
-		readee = fopen(argv[i],"rb");
-		if (readee == NULL) {
-			perror("Unable to open file");
-			return EXIT_FAILURE;
+	if (!isInteger) {
+		unsigned char byte;
+		for (i=1;i<argc;++i) {	
+			readee = fopen(argv[i],"rb");
+			if (readee == NULL) {
+				perror("Unable to open file");
+					return EXIT_FAILURE;
+			}
+			while (fread(&byte,sizeof(unsigned char),1,readee) == 1) {
+				printBianary(byte);
+			}
+			printf("\n");
+			fclose(readee);
 		}
-		while (fread(&byte,sizeof(unsigned char),1,readee) == 1) {
-			printBianary(byte);
-		}
-		printf("\n");
-		fclose(readee);
 	}
-	for (i=1;i<argc;++i) {
-		readee = fopen(argv[i],"rb");	
-		if (readee == NULL) {
-			perror("Unable to open file");
-			return EXIT_FAILURE;
-		}	
-		fseek(readee,0,SEEK_END);
-		long size = ftell(readee);
-		int *data = (int *)malloc(size);
-		if (data==NULL) {
-			perror("Memory allocation failed");
-			free(data);
-			fclose(readee);
-			return EXIT_FAILURE;
-		}
-		rewind(readee);
-		size_t datasz = fread(data,sizeof(int),size,readee);
-		if (datasz != size) {
-			if (ferror(readee)) {
-				perror("Error reading file");
+	else {
+		for (i=1;i<argc;++i) {
+			readee = fopen(argv[i],"rb");	
+			if (readee == NULL) {
+				perror("Unable to open file");
+				return EXIT_FAILURE;
+			}	
+			fseek(readee,0,SEEK_END);
+			long size = ftell(readee);
+			int *data = (int *)malloc(size);
+			if (data==NULL) {
+				perror("Memory allocation failed");
+				free(data);
+				fclose(readee);
+				return EXIT_FAILURE;
 			}
-			else {
-				printf("Possible file reading error or size conflict");
+			rewind(readee);
+			size_t datasz = fread(data,sizeof(int),size,readee);
+			if (datasz != size) {
+				if (ferror(readee)) {
+					perror("Error reading file");
 			}
+				else {
+					printf("Possible file reading error or size conflict");
+				}
+				fclose(readee);
+				free(data);
+				return EXIT_FAILURE;
+			}
+			fread(data,sizeof(int),size,readee);
+			for (int i2=0;i2<size;++i2) {
+				printf("%d",data[i2]);
+			}
+			printf("\n");
 			fclose(readee);
 			free(data);
-			return EXIT_FAILURE;
+			data = NULL;
 		}
-		fread(data,sizeof(int),size,readee);
-		for (int i2=0;i2<size;++i2) {
-			printf("%d",data[i2]);
-		}
-		printf("\n");
-		fclose(readee);
-		free(data);
-		data = NULL;
 	}
 	return EXIT_SUCCESS;
 }
