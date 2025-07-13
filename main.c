4660774a8494d8412c6677304d0e28dc8ccdf446
ajassi9751 <ajassi9751@gmail.com>
2025-07-13 02:45:57 -0400

    Correctly implemented bianary reading, includes integer reading but needs a flag and a possible length flag

--- main.c
+++ main.c
@@ -2,21 +2,71 @@
 #include<stdlib.h>
 #include<string.h>
 
-int main() {
-	char path[256];
-	scanf("%255s", path);
-	FILE *readee;
-	readee = fopen(path,"rb");
-	
-	fseek(readee,0,SEEK_END);
-	long size = ftell(readee);
-	rewind(readee);
-	int data[size];
-	fread(data,sizeof(int),size,readee);
-	fclose(readee);
+void printBianary (unsigned char byte) {
 	int i;
-	for(i=0;i<size;++i) {
-		printf("%d", data[i]);
+	for (i = 7; i>=0;--i) {
+		printf("%d",(byte>>i)&1);
 	}
-	return 1;
+}
+
+
+int main (int argc, char *argv[]) {
+	char path[256];
+	if (argc<2) {
+		perror("Usage: catb <filename> <filename> <filename> ...");
+		return EXIT_FAILURE;
+	}
+	FILE *readee;
+	unsigned char byte;
+	int i;
+	for (i=1;i<argc;++i) {	
+		readee = fopen(argv[i],"rb");
+		if (readee == NULL) {
+			perror("Unable to open file");
+			return EXIT_FAILURE;
+		}
+		while (fread(&byte,sizeof(unsigned char),1,readee) == 1) {
+			printBianary(byte);
+		}
+		printf("\n");
+		fclose(readee);
+	}
+	for (i=1;i<argc;++i) {
+		readee = fopen(argv[i],"rb");	
+		if (readee == NULL) {
+			perror("Unable to open file");
+			return EXIT_FAILURE;
+		}	
+		fseek(readee,0,SEEK_END);
+		long size = ftell(readee);
+		int *data = (int *)malloc(size);
+		if (data==NULL) {
+			perror("Memory allocation failed");
+			free(data);
+			fclose(readee);
+			return EXIT_FAILURE;
+		}
+		rewind(readee);
+		size_t datasz = fread(data,sizeof(int),size,readee);
+		if (datasz != size) {
+			if (ferror(readee)) {
+				perror("Error reading file");
+			}
+			else {
+				printf("Possible file reading error or size conflict");
+			}
+			fclose(readee);
+			free(data);
+			return EXIT_FAILURE;
+		}
+		fread(data,sizeof(int),size,readee);
+		for (int i2=0;i2<size;++i2) {
+			printf("%d",data[i2]);
+		}
+		printf("\n");
+		fclose(readee);
+		free(data);
+		data = NULL;
+	}
+	return EXIT_SUCCESS;
 }
